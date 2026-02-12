d06fb3d71220e187f6aa752a5b84d31f925c3f4a
Alireza Jazayeri <144585078+Alireza-jaz@users.noreply.github.com>
2026-02-13 00:58:16 +0330

    Update WorkersWithTheHighestSalaries.sql
    Management wants to analyze only employees with official job titles. Find the job titles of the employees with the highest salary.If multiple employees have the same highest salary, include all their job titles.

--- SQL/Easy/WorkersWithTheHighestSalaries.sql
+++ SQL/Easy/WorkersWithTheHighestSalaries.sql
@@ -1,1 +1,22 @@
+-- Title: Workers With The Highest Salaries
+-- Difficulty: Easy
+--Question: Management wants to analyze only employees with official job titles. Find the job titles of the employees with the highest salary.
+--             If multiple employees have the same highest salary, include all their job titles.
 
+--First Solution
+SELECT tit.worker_title AS best_paid_title
+FROM worker AS emp 
+JOIN title AS tit ON emp.worker_id=tit.worker_ref_id
+WHERE (emp.salary=(SELECT MAX(salary) FROM worker emp
+    JOIN title AS tit ON emp.worker_id=tit.worker_ref_id))
+
+
+--Second Solution 
+WITH max_salary AS(
+    SELECT MAX(salary) FROM worker emp
+        JOIN title AS tit ON emp.worker_id=tit.worker_ref_id)
+  
+SELECT tit.worker_title AS best_paid_title
+FROM worker AS emp 
+JOIN title AS tit ON emp.worker_id=tit.worker_ref_id
+WHERE emp.salary= (SELECT * FROM max_salary)
