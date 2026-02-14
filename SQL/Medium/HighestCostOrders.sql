05af8a9772db963f2b41519c8615180eeb235c5c
Alireza Jazayeri <144585078+Alireza-jaz@users.noreply.github.com>
2026-02-14 19:55:52 +0330

    Create HighestCostOrders.sql

---
+++ SQL/Medium/HighestCostOrders.sql
@@ -0,0 +1,9 @@
+-- Title: Highest Cost Orders
+-- Difficulty: Medium
+--Question: 
+      /*Find the customers with the highest daily total order cost between 2019-02-01 and 2019-05-01. If a customer had more than one order on a certain day, 
+        sum the order costs on a daily basis. Output each customer's first name, total cost of their items, and the date.
+        If multiple customers tie for the highest daily total on the same date, return all of them.
+        For simplicity, you can assume that every first name in the dataset is unique.*/
+
+--First Solution
